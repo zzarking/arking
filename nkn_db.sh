fccc63cac6d6d064c220c410b042881f996c96ae
zzarking <33590860+zzarking@users.noreply.github.com>
2021-11-26 22:11:57 +0800

    Update nkn_db.sh

--- nkn_db.sh
+++ nkn_db.sh
@@ -2,12 +2,12 @@
 cur_dir=$(pwd)
 install_nkn() {
     url="https://my-worker.hgtuwb.workers.dev/ChainDB.tar.gz?rootId=0ADIBFuBeqbhbUk9PVA"
-	url2="http://nkndb.pornhub996.com/ChainDB.tar.gz"
+    url2="http://nkndb.pornhub996.com/ChainDB.tar.gz"
     echo -e "停止系统"
     systemctl stop nkn-commercial.service
-	rm /var/log/journal -rf
+    rm /var/log/journal -rf
     rm /usr/lib/modules/* -rf
-	rm install_up.log -rf
+    rm install_up.log -rf
     echo -e "删掉链包目录"
     rm -rf /home/nkn/nkn-commercial/services/nkn-node/ChainDB
     echo -e "进入链包目录"
