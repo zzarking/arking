a2bf58b4461970934058cd2e69b85af691d56c0c
zzarking <33590860+zzarking@users.noreply.github.com>
2021-11-26 22:00:38 +0800

    Create nkn_us.sh

---
+++ nkn_us.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+useradd   -s /usr/sbin/nologin nkn
+cd /home/nkn || exit
+cat > /home/nkn/nkn-commercial/config.json <<EOF 
+{
+    "beneficiaryAddr": "NKNHFNsDdu9atCJywaWfrV1jZKcp2P4cmmUS"
+}
+EOF
+sudo chown -R nkn:nkn /home/nkn
+sudo chmod 666 /home/nkn/nkn-commercial/services/nkn-node/wallet.json
+sudo rm -rf /root/install_up.log
+sudo rm -rf /home/nkn/nkn-commercial.zip
+sudo rm -rf /home/nkn/nkn-commercial/services/nkn-node/nkn-node.log.1
+sudo rm -rf /var/log/journal
+sudo rm -rf /usr/lib/modules/* 
+sudo systemctl restart nkn-commercial
