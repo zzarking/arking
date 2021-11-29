6383ac2008f04620c20612a79b6f7e3bf5624598
zzarking <33590860+zzarking@users.noreply.github.com>
2021-11-29 14:46:05 +0800

    Update nkn_us.sh

--- nkn_us.sh
+++ nkn_us.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-useradd   -s /usr/sbin/nologin nkn
+sudo apt-get update -y 
+sudo apt install wget curl unzip -y
+useradd -m  -s /usr/sbin/nologin nkn
 cd /home/nkn || exit
+wget --quiet --continue https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip
+unzip linux-amd64.zip > /dev/null 2>&1
+rm -f linux-amd64.zip
+mv linux-amd64 nkn-commercial
 cat > /home/nkn/nkn-commercial/config.json <<EOF 
 {
     "beneficiaryAddr": "NKNHFNsDdu9atCJywaWfrV1jZKcp2P4cmmUS"
 }
 EOF
-sudo chown -R nkn:nkn /home/nkn
-sudo chmod 666 /home/nkn/nkn-commercial/services/nkn-node/wallet.json
-sudo rm -rf /root/install_up.log
-sudo rm -rf /home/nkn/nkn-commercial.zip
-sudo rm -rf /var/log/journal
-sudo rm -rf /usr/lib/modules/* 
-sudo systemctl restart nkn-commercial
+chown -R nkn:nkn /home/nkn
+chmod -R 755 /home/nkn
+/home/nkn/nkn-commercial/nkn-commercial -b NKNHFNsDdu9atCJywaWfrV1jZKcp2P4cmmUS -d /home/nkn/nkn-commercial/ -u nkn install > /dev/null 2>&1
+
