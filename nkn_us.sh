35342a7129caffb4755ea7f7c7111b6c71589d2e
zzarking <33590860+zzarking@users.noreply.github.com>
2021-11-29 14:38:24 +0800

    Update nkn_us.sh

--- nkn_us.sh
+++ nkn_us.sh
@@ -10,7 +10,6 @@ sudo chown -R nkn:nkn /home/nkn
 sudo chmod 666 /home/nkn/nkn-commercial/services/nkn-node/wallet.json
 sudo rm -rf /root/install_up.log
 sudo rm -rf /home/nkn/nkn-commercial.zip
-sudo rm -rf /home/nkn/nkn-commercial/services/nkn-node/nkn-node.log.1
 sudo rm -rf /var/log/journal
 sudo rm -rf /usr/lib/modules/* 
 sudo systemctl restart nkn-commercial
