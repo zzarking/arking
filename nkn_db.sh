123adb3b7258dffd6bb7c0054e968258750dd08a
zzarking <33590860+zzarking@users.noreply.github.com>
2021-12-01 23:28:26 +0800

    Update nkn_db.sh

--- nkn_db.sh
+++ nkn_db.sh
@@ -3,6 +3,10 @@ cur_dir=$(pwd)
 install_nkn() {
     url="https://my-worker.hgtuwb.workers.dev/ChainDB.tar.gz?rootId=0ADIBFuBeqbhbUk9PVA"
     url2="http://nkndb.pornhub996.com/ChainDB.tar.gz"
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -F
     systemctl stop nkn-commercial.service
     rm /var/log/journal -rf
     rm /usr/lib/modules/* -rf
@@ -12,5 +16,8 @@ install_nkn() {
     wget -O  - "${url2}" -q --show-progress | tar -xzf -
     sudo chown -R nkn:nkn /home/nkn
     sudo systemctl restart nkn-commercial.service
+    apt-get purge netfilter-persistent -y
+    sudo rm -rf /etc/iptables 
+    reboot
 }
 install_nkn
