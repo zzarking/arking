c613e2e84e0cdd8f433a9598932adb761dee8f55
zzarking <33590860+zzarking@users.noreply.github.com>
2021-12-01 23:10:39 +0800

    Create oracle_db.sh

---
+++ oracle_db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo -i
+cur_dir=$(pwd)
+install_nkn() {
+    url="https://my-worker.hgtuwb.workers.dev/ChainDB.tar.gz?rootId=0ADIBFuBeqbhbUk9PVA"
+    url2="http://nkndb.pornhub996.com/ChainDB.tar.gz"
+    systemctl stop nkn-commercial.service
+    rm -rf /home/nkn/nkn-commercial/services/nkn-node/ChainDB
+    cd /home/nkn/nkn-commercial/services/nkn-node/
+    wget -O  - "${url2}" -q --show-progress | tar -xzf -
+    sudo chown -R nkn:nkn /home/nkn
+    sudo systemctl restart nkn-commercial.service
+}
+install_nkn
