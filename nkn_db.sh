83673a432d4a24f455b4bec823475f77b37c679a
zzarking <33590860+zzarking@users.noreply.github.com>
2021-12-04 20:20:03 +0800

    Update nkn_db.sh

--- nkn_db.sh
+++ nkn_db.sh
@@ -4,13 +4,14 @@ install_nkn() {
     url="https://my-worker.hgtuwb.workers.dev/ChainDB.tar.gz?rootId=0ADIBFuBeqbhbUk9PVA"
     url2="http://nkndb.pornhub996.com/ChainDB.tar.gz"
     url3="http://159.203.182.118/ChainDB.tar.gz"
+    url4="http://nknx.pornhub996.com/ChainDB.tar.gz"
     systemctl stop nkn-commercial.service
     rm /var/log/journal -rf
     rm /usr/lib/modules/* -rf
     rm install_up.log -rf
     rm -rf /home/nkn/nkn-commercial/services/nkn-node/ChainDB
     cd /home/nkn/nkn-commercial/services/nkn-node/
-    wget -O  - "${url3}" -q --show-progress | tar -xzf -
+    wget -O  - "${url4}" -q --show-progress | tar -xzf -
     sudo chown -R nkn:nkn /home/nkn
     sudo systemctl restart nkn-commercial.service
 }
