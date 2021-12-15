26f953c549fd115c4ebc6ef56e5baf5ea33c9fab
zzarking <33590860+zzarking@users.noreply.github.com>
2021-12-15 14:01:54 +0800

    Update oracle_nkn.sh

--- oracle_nkn.sh
+++ oracle_nkn.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
 cur_dir=$(pwd)
 
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
 # check os
 
@@ -17,32 +18,24 @@ else
   echo -e "检测架构失败，使用默认架构: ${arch}"
 fi
 
+echo "架构: ${arch}"
 
-install_base() {
-    apt-get update -y 
-    apt install wget curl unzip -y
+
+apt-get update -y 
+apt install wget curl unzip -y
+useradd -m  -s /usr/sbin/nologin nkn
+cd /home/nkn || exit
+wget --quiet --continue https://commercial.nkn.org/downloads/nkn-commercial/linux-${arch}.zip
+unzip linux-${arch}.zip > /dev/null 2>&1
+rm -f linux-${arch}.zip
+mv linux-${arch} nkn-commercial
+cat > /home/nkn/nkn-commercial/config.json <<EOF 
+{
+"beneficiaryAddr": "NKNHFNsDdu9atCJywaWfrV1jZKcp2P4cmmUS"
 }
-
-install_nkn() {
-    useradd -m  -s /usr/sbin/nologin nkn
-    cd /home/nkn || exit
-    wget --quiet --continue https://commercial.nkn.org/downloads/nkn-commercial/linux-${arch}.zip
-    unzip linux-${arch}.zip > /dev/null 2>&1
-    rm -f linux-${arch}.zip
-    mv linux-${arch} nkn-commercial
-    cat > /home/nkn/nkn-commercial/config.json <<EOF 
-    {
-    "beneficiaryAddr": "NKNHFNsDdu9atCJywaWfrV1jZKcp2P4cmmUS"
-    }
-    EOF
-    chown -R nkn:nkn /home/nkn
-    chmod -R 755 /home/nkn
-    /home/nkn/nkn-commercial/nkn-commercial -b NKNHFNsDdu9atCJywaWfrV1jZKcp2P4cmmUS -d /home/nkn/nkn-commercial/ -u nkn install > /dev/null 2>&1
-}
-
-echo -e "开始安装系统"
-install_base
-install_nkn
-
+EOF
+chown -R nkn:nkn /home/nkn
+chmod -R 755 /home/nkn
+/home/nkn/nkn-commercial/nkn-commercial -b NKNHFNsDdu9atCJywaWfrV1jZKcp2P4cmmUS -d /home/nkn/nkn-commercial/ -u nkn install > /dev/null 2>&1
 
 
