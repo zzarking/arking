39a04e6bbd12d3e1cb5f1540a734ecdff5b204a5
zzarking <33590860+zzarking@users.noreply.github.com>
2021-12-16 23:23:09 +0800

    Update sk5.sh

--- sk5.sh
+++ sk5.sh
@@ -23,7 +23,7 @@ echo "架构: ${arch}"
 wget -N --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v2.11.1/${arch}.gz && gzip -d ${arch}.gz
 mv ${arch} /usr/bin/gost
 chmod +x /usr/bin/gost
-gost -L=socks5://:2021
+gost -L arking:3321444a@:2021 socks5://:2021
 
 
 
