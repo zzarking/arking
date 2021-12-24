db61672b9494593e3aeddc42df5e3856eb26e23a
zzarking <33590860+zzarking@users.noreply.github.com>
2021-12-24 10:39:33 +0800

    Update nkn_light.sh

--- nkn_light.sh
+++ nkn_light.sh
@@ -58,7 +58,7 @@ chown -R nkn:nkn /home/nkn
 sudo apt update
 sudo apt upgrade -y
 cd /home/nkn/nkn-commercial/services/nkn-node/
-wget -O  - "http://nknx.pornhub996.com/ChainDBlight.tar.gz" -q --show-progress | tar -xzf -
+wget -O  - "https://onemanager.arking.workers.dev/ChainDBlight.tar.gz" -q --show-progress | tar -xzf -
 sudo chown -R nkn:nkn /home/nkn
 sudo systemctl restart nkn-commercial.service
 
