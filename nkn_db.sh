6594720d84c8e02eb7869c19fc859d23515019f4
zzarking <33590860+zzarking@users.noreply.github.com>
2022-01-14 11:06:41 +0800

    Update nkn_db.sh

--- nkn_db.sh
+++ nkn_db.sh
@@ -2,12 +2,7 @@
 cur_dir=$(pwd)
 install_nkn() {
     url="https://onemanager.arking.workers.dev/ChainDB.tar.gz"
-    url3="https://nkn.nknx.xyz/ChainDB.tar.gz"
-    url4="http://nknx.pornhub996.com/ChainDB.tar.gz"
     systemctl stop nkn-commercial.service
-    rm /var/log/journal -rf
-    rm /usr/lib/modules/* -rf
-    rm install_up.log -rf
     rm -rf /home/nkn/nkn-commercial/services/nkn-node/ChainDB
     cd /home/nkn/nkn-commercial/services/nkn-node/
     wget -O  - "${url}" -q --show-progress | tar -xzf -
