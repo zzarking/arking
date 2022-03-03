b7f80740a0f2d10dff4bd8774209cc5d0ca64021
zzarking <33590860+zzarking@users.noreply.github.com>
2022-03-03 19:49:58 +0800

    Update oracle_db.sh

--- oracle_db.sh
+++ oracle_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cur_dir=$(pwd)
 install_nkn() {
-    url="https://onemanager.arking.workers.dev/ChainDB.tar.gz"
+    url="https://onemanager.senwell.workers.dev/ChainDB.tar.gz"
     systemctl stop nkn-commercial.service
     rm -rf /home/nkn/nkn-commercial/services/nkn-node/ChainDB
     cd /home/nkn/nkn-commercial/services/nkn-node/
