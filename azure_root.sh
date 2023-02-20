4c630095b8e5fd6cfeb50f94e0c5fc78d5d35612
zzarking <33590860+zzarking@users.noreply.github.com>
2023-02-20 09:43:17 +0800

    Update and rename azure_root to azure_root.sh

--- azure_root.sh
+++ azure_root.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 sudo -i
-echo root:KmhBzdhb2uEJU5nknDD |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo reboot
