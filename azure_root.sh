00d1899a02eeae70a21e1d07b579825cec73e111
zzarking <33590860+zzarking@users.noreply.github.com>
2023-02-20 09:49:13 +0800

    Update azure_root.sh

--- azure_root.sh
+++ azure_root.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo -i
+echo root:KmhBzdhb2uEJU5l844fFGgG |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo reboot
