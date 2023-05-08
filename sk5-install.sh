fcb6ca0dba98f1da0673bea841fae60cf5a4ba35
zzarking <33590860+zzarking@users.noreply.github.com>
2023-05-08 13:25:41 +0800

    Update sk5-install.sh

--- sk5-install.sh
+++ sk5-install.sh
@@ -12,7 +12,7 @@ Wants=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/local/bin/gost -L 123:123@:1080 socks5://:1080
+ExecStart=/usr/local/bin/gost -L 123:123@:443 socks5://:443
 Restart=always
 
 [Install]
