38e9e7b80840b6e0df9042c0403ba3b0352e71f0
zzarking <33590860+zzarking@users.noreply.github.com>
2024-01-09 10:58:13 +0800

    Update meson.sh

--- meson.sh
+++ meson.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+sudo apt-get update -y && sudo apt-get install wget -y
+
+cd ~ 
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.20/meson_cdn-linux-amd64.tar.gz'
+tar -zxf meson_cdn-linux-amd64.tar.gz
+rm -f meson_cdn-linux-amd64.tar.gz
+cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
+sleep 10
+./meson_cdn config set --token=kdjxmjwhzewvrupjda35cdbbdbe5fa8e --https_port=443 --cache.size=30
+sleep 10
+./service start meson_cdn
+sleep 10
+./service status meson_cdn
