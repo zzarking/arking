63d8dead54e8e83d868a5507949c10a07e0079ea
zzarking <33590860+zzarking@users.noreply.github.com>
2024-01-20 16:35:39 +0800

    Update gaga.sh

--- gaga.sh
+++ gaga.sh
@@ -14,10 +14,22 @@ cd ./apphub-linux-amd64
 sleep 10
 ./apphub status
 sleep 10
-./apps/gaganode/gaganode config set --token=dlrdjohudniaswpad604f27514f17a80
+./apps/gaganode/gaganode config set --token=zjpidsmxacrwvwsj6f677b11c926cbff
 ./apphub restart
 sleep 20
 ./apphub status
 ./apphub log
 sleep 70
 sudo systemctl start gost.service
+sleep 10
+cd ~ 
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.20/meson_cdn-linux-amd64.tar.gz'
+tar -zxf meson_cdn-linux-amd64.tar.gz
+rm -f meson_cdn-linux-amd64.tar.gz
+cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
+sleep 10
+./meson_cdn config set --token=kdjxmjwhzewvrupjda35cdbbdbe5fa8e --https_port=443 --cache.size=30
+sleep 10
+./service start meson_cdn
+sleep 10
+./service status meson_cdn
