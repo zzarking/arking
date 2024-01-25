924b8fa6afdee4ddc0b8f372b283417040a32bc6
zzarking <33590860+zzarking@users.noreply.github.com>
2024-01-25 12:47:09 +0800

    Update gcp.sh

--- gcp.sh
+++ gcp.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo apt-get update -y && sudo apt-get install wget -y
-
+sudo apt-get update -y && sudo apt-get install wget curl gzip -y
 
 cd ~
-
 curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz
 tar -zxf apphub-linux-amd64.tar.gz
 rm -f apphub-linux-amd64.tar.gz
@@ -32,3 +30,31 @@ sleep 10
 ./service start meson_cdn
 sleep 10
 ./service status meson_cdn
+
+
+
+wget https://github.com/meyuteng/meyuteng/raw/main/gost-linux-amd64-2.11.1.gz
+gunzip gost-linux-amd64-2.11.1.gz || gzip gost-linux-amd64-2.11.1.gz
+mv gost-linux-amd64-2.11.1 /usr/local/bin/gost || mv gost-linux-amd64-2.11.1.gz /usr/local/bin/gost
+chmod +x /usr/local/bin/gost
+cat > /etc/systemd/system/gost.service << EOF
+[Unit]
+Description=Gost Proxy
+After=network.target
+Wants=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/gost -L 123:123@:1080 socks5://:1080
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+
+systemctl start gost.service
+
+systemctl enable gost.service
+
+echo "The installation is complete"
