3673f51d8434d513389f5452b42e16a9572e2703
zzarking <33590860+zzarking@users.noreply.github.com>
2024-06-17 09:03:01 +0800

    Update meson_ok.sh

--- meson_ok.sh
+++ meson_ok.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-cd /home/meson_cdn-linux-amd64
+cd /home
 sleep 1
-./service stop meson_cdn
-sleep 5
-./meson_cdn config set --token=jwoouwozukwovvvs2179f2b29421461b --https_port=443 --cache.size=30
+cd ./meson_cdn-linux-amd64 && sudo ./service stop meson_cdn && sudo ./service remove meson_cdn
+
+
+
+
+
 sleep 10
-./service restart meson_cdn
+cd /home
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.20/meson_cdn-linux-amd64.tar.gz'
+tar -zxf meson_cdn-linux-amd64.tar.gz
+rm -f meson_cdn-linux-amd64.tar.gz
+cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
+sleep 10
+./meson_cdn config set --token=imgjsnuklfnduwxo7513b3cc99b43616 --https_port=443 --cache.size=30
+sleep 10
+./service start meson_cdn
 sleep 10
 ./service status meson_cdn
